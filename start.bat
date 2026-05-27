9bef2b3c9ef6ffc326dc90d38faa2dca8be47ecd
unknown <yoonchaebabo@outlook.kr>
2026-05-27 16:33:22 +0900

    Auto Save

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
-git pull
-java -Xms4G -Xmx16G -jar paper-1.21.11-132.jar nogui
+rem git pull
+java -Xms16G -Xmx16G -jar paper-1.21.11-132.jar nogui
 git add .
 git commit -m "Auto Save"
 git push
