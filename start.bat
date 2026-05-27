43513cfb1ba9e1ea0999feaa9b19eba445575dad
unknown <yoonchaebabo@outlook.kr>
2026-05-27 16:47:49 +0900

    Auto Save

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 rem git pull
-java -Xms16G -Xmx16G -jar paper-1.21.11-132.jar nogui
+java -Xms12G -Xmx12G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=20 -XX:+UnlockExperimentalVMOptions -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8m -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -jar paper-1.21.11-132.jar nogui
 git add .
 git commit -m "Auto Save"
 git push
